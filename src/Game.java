e5603b30ae2f916145c1f0daa79f7eba75ea41bb
Andrew <excisionhd1@gmail.com>
2018-10-07 16:57:32 -0700

    updated genetic algorithm

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,10 @@
+import com.sun.tools.javah.Gen;
 import javafx.collections.transformation.SortedList;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Game {
-    
+
     // generates random initial state
     public static int[] generateRandomState(int n) {
 
@@ -34,6 +32,13 @@ public class Game {
     public static void main(String[] args) {
         SimulatedAnnealing s = new SimulatedAnnealing();
         s.solve(21,100000,9900,0.5);
+
+
+        int[] test = {16,12,8,18,2,7,1,4,20,10,17,14,3,13,19,5,15,0,6,11,9};
+
+        GeneticAlgorithm g = new GeneticAlgorithm();
+        g.solve(21,100,0.1,100, 3);
+        //System.out.println(computeCost(test));
     }
 
 }
@@ -108,6 +113,152 @@ class SimulatedAnnealing {
 
 class GeneticAlgorithm{
 
+    public static ArrayList<int[]> generatePopulation(int boardSize, int pSize){
+
+        ArrayList<int[]> p = new ArrayList<>();
+        for (int i = 0; i<pSize;i++){
+            int[] board;
+            board = Game.generateRandomState(boardSize);
+            p.add(board);
+            //printArray(board);
+            //System.out.print(Game.computeCost(board) + "\n");
+            //printArray(board);
+        }
+
+        return  p;
+
+    }
+
+    public static void printArray(int[] array){
+        for (int i = 0;i<array.length;i++){
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int computeFitness(int[] array){
+
+
+        int maxAttackingPairs = (array.length * (array.length - 1))/2;
+        int numAttackingPairs = computeAttackingPairs(array);
+
+        return maxAttackingPairs - numAttackingPairs;
+
+    }
+
+    public static int computeAttackingPairs(int[] r) {
+        int h = 0;
+
+        // increment cost if two queens are in same row or in same diagonal.
+        for (int i = 0; i < r.length; i++)
+            for (int j = i + 1; j < r.length; j++)
+                if (r[i] == r[j] || Math.abs(r[i] - r[j]) == j - i)
+                    h += 1;
+
+        return h;
+    }
+
+    public static ArrayList<int[]> getSortedPopulation(ArrayList<int[]> population){
+        PriorityQueue<int[]> sortedPop = new PriorityQueue<>(population.size(), new SortByFitness());
+
+        for (int i = 0; i<population.size();i++){
+            sortedPop.add(population.get(i));
+        }
+
+        ArrayList<int[]> sortedPopulation = new ArrayList<>();
+
+        for (int j = 0; j<sortedPop.size();j++){
+            int[] curr;
+            curr = sortedPop.poll();
+            sortedPopulation.add(curr);
+        }
+
+
+        for (int i = 0; i<sortedPopulation.size();i++){
+            //printArray(sortedPopulation.get(i));
+            //System.out.println(computeFitness(sortedPopulation.get(i)));
+        }
+
+        return sortedPopulation;
+
+
+    }
+
+    public static int[] solve(int boardSize, int populationSize, double mutationChance, int numGenerations, int kWay){
+        ArrayList<int[]> population = generatePopulation(boardSize, populationSize);
+        ArrayList<int[]> newPopulation = new ArrayList<>();
+        //ArrayList<int[]> sorted = getSortedPopulation(population);
+        int bestFit = (boardSize * (boardSize -1))/2;
+
+        for (int i = 0;i <population.size();i++){
+            if (computeFitness(population.get(i))== bestFit){
+                return population.get(i);
+            }
+        }
+
+        for (int i = 0; i<numGenerations;i++){
+
+            newPopulation.clear();
+            for (int pop = 0; pop<populationSize;pop++){
+                ArrayList<int[]> selectedParents = new ArrayList<>();
+
+                //select 3 random from the population
+                for (int j = 0; j<kWay;j++){
+                    int randomChild = (int) (Math.random() * populationSize);
+                    selectedParents.add(population.get(randomChild));
+                }
+
+                //sort the parents and get top 2 fitness
+                ArrayList<int[]> sortedParents = getSortedPopulation(selectedParents);
+                int[] parent1, parent2;
+                parent1 = sortedParents.get(0);
+                parent2 = sortedParents.get(1);
+
+                //crossover
+                ArrayList<int[]> crossChildren = crossover(parent1, parent2);
+
+                newPopulation.add(crossChildren.get(0));
+                newPopulation.add(crossChildren.get(1));
+
+            }
+
+        }
+
+
+        return null;
+    }
+
+    public static ArrayList<int[]> crossover(int[] a, int[] b){
+        int size = a.length;
+        int crossPos = (int) (Math.random() * size);
+        int[] child1 = new int[size];
+        int[] child2 = new int[size];
+        ArrayList<int[]> children = new ArrayList<>();
+
+        System.arraycopy(a, 0, child1, 0, crossPos);
+        System.arraycopy(b, crossPos, child1, crossPos, size-crossPos);
+        System.arraycopy(b, 0, child2, 0, crossPos);
+        System.arraycopy(a, crossPos, child2, crossPos, size-crossPos);
+
+        children.add(child1);
+        children.add(child2);
+
+        return children;
+    }
+
+}
+
+class SortByFitness implements Comparator<int[]>{
+    public int compare(int[] a, int[] b){
+        if (GeneticAlgorithm.computeFitness(a) < GeneticAlgorithm.computeFitness(b)){
+            return 1;
+
+        }
+        else if (GeneticAlgorithm.computeFitness(a) > GeneticAlgorithm.computeFitness(b)){
+            return -1;
+        }
+        return 0;
+    }
 }
 
 
